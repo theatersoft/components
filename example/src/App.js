45d9e481df467a9c8b7daa7e8f245aaeffd5a2f4
Rob Huehn <rhuehn@gmail.com>
2017-04-10 13:45:10 -0700

    create-react-app examples wip

---
+++ example/src/App.js
@@ -0,0 +1,16 @@
+import { h, Component } from 'preact';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p class="App-intro">
+          get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ example2/README.md
@@ -0,0 +1,12 @@
+This project was bootstrapped with:
+
+```
+create-react-app example --scripts-version @just-boris/preact-scripts
+```
+
+Additional dependencies:
+```
+preact-compat
+md-components
+react-router-dom
+```
