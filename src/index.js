b2dba17c6f4212a545504f391c9ccc7c1645cbb5
Rob Huehn <rhuehn@gmail.com>
2017-04-12 13:09:57 -0700

    rm duplication in exported component styl

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import {h, render, Component} from 'preact'
+import './index.styl'
 
+import {h, render, Component} from 'preact'
 // https://preactjs.com/guide/switching-to-preact
 const rerender = (n, p = document.getElementById('ui'), r = document.getElementById('ui').lastChild) => render(n, p, r)
 export {rerender as render}
