c1260b6df89a697ad6195e4443fdb589d0ebb08e
Rob Huehn <rhuehn@gmail.com>
2017-04-25 19:19:48 -0700

    add icon spec

--- spec/src/App.js
+++ spec/src/App.js
@@ -1,12 +1,14 @@
 import {h} from 'preact'
 import '@theatersoft/components/components.css'
 import Button from './components/button'
+import Icon from './components/icon'
 import Switch from './components/switch'
 import './App.styl'
 
 export default () => (
     <div>
         <Switch/>
+        <Icon/>
         <Button/>
     </div>
 )
---
+++ spec/src/components/icon.js
@@ -0,0 +1,9 @@
+import {h} from 'preact'
+import {Icon} from '@theatersoft/components'
+
+export default () => (
+    <section>
+        <Icon icon="arrow-up"/>
+        <Icon icon="arrow-down"/>
+    </section>
+)
