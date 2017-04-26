2da1f9028aa267b6e5e052ca550a2bdba1dfbc59
Rob Huehn <rhuehn@gmail.com>
2017-04-25 19:11:18 -0700

    index source rename

--- src/icon/index.js
+++ src/icon/index.js
@@ -1,9 +1,1 @@
-import {h, Component} from 'preact'
-import './index.styl'
-
-export const Icon = ({icon, cb}) =>
-    <div class="iconpad" onClick={e => {e.stopPropagation(); cb()}}>
-        <svg class="icon" id={`icon-${icon}`}>
-            <use href={`#svg-${icon}`}/>
-        </svg>
-    </div>
+export {Icon} from './Icon'
