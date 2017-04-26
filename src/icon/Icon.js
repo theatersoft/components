fd315591f0d99009c14f84c012693ffb01fff995
Rob Huehn <rhuehn@gmail.com>
2017-04-26 12:01:54 -0700

    style icon smaller and inline

--- src/icon/Icon.js
+++ src/icon/Icon.js
@@ -2,8 +2,8 @@ import {h, Component} from 'preact'
 import './icon.styl'
 
 export const Icon = ({icon, cb}) =>
-    <div class="iconpad" onClick={e => {e.stopPropagation(); cb()}}>
-        <svg class="icon" id={`icon-${icon}`}>
+    <div class="icon" onClick={e => {e.stopPropagation(); cb()}}>
+        <svg id={`icon-${icon}`}>
             <use href={`#svg-${icon}`}/>
         </svg>
     </div>
