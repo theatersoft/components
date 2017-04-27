93329709c8e48a28ad2d9eb3a8a9623cfb4d7346
Rob Huehn <rhuehn@gmail.com>
2017-04-27 13:04:57 -0700

    guard cb

--- src/icon/Icon.js
+++ src/icon/Icon.js
@@ -2,7 +2,7 @@ import {h, Component} from 'preact'
 import './icon.styl'
 
 export const Icon = ({icon, cb}) =>
-    <div class="icon" onClick={e => {e.stopPropagation(); cb()}}>
+    <div class="icon" onClick={e => {e.stopPropagation(); cb && cb()}}>
         <svg id={`icon-${icon}`}>
             <use href={`#svg-${icon}`}/>
         </svg>
