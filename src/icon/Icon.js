6a33b7ee0a4b332dfe5ff3b6aa02ca519384e9f7
Rob Huehn <rhuehn@gmail.com>
2017-04-27 14:54:24 -0700

    small icons

--- src/icon/Icon.js
+++ src/icon/Icon.js
@@ -1,9 +1,18 @@
 import {h, Component} from 'preact'
+import {classes} from '../classes'
 import './icon.styl'
 
-export const Icon = ({icon, cb}) =>
-    <div class="icon" onClick={e => {e.stopPropagation(); cb && cb()}}>
-        <svg id={`icon-${icon}`}>
-            <use href={`#svg-${icon}`}/>
-        </svg>
-    </div>
+export class Icon extends Component {
+    render ({icon, small, cb}) {
+        return (
+            <div
+                class={classes('icon', small && 'small')}
+                onClick={e => {e.stopPropagation(); cb && cb()}}
+            >
+                <svg id={`icon-${icon}`}>
+                    <use href={`#svg-${icon}`}/>
+                </svg>
+            </div>
+        )
+    }
+}
