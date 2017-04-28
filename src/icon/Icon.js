571f0c6a3b8e2ae7eb104cc8d593acc1f7234a12
Rob Huehn <rhuehn@gmail.com>
2017-04-28 14:54:14 -0700

    icon cleanup

--- src/icon/Icon.js
+++ src/icon/Icon.js
@@ -3,15 +3,12 @@ import {classes} from '../classes'
 import './icon.styl'
 
 export class Icon extends Component {
-    //render ({icon, small, cb, className}) {
     render (props) {
         const {icon, small, cb} = props
-        console.log(props)
         return (
             <span
                 class={classes('icon', props.class, small && 'small')}
-                onClick={e => {e.stopPropagation(); cb && cb()}}
-            >
+                onClick={cb}>
                 <svg id={`icon-${icon}`}>
                     <use href={`#svg-${icon}`}/>
                 </svg>
