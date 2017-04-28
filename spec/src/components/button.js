3cb6723c996cc7a8c387231b988e1dcdcc99b2f6
Rob Huehn <rhuehn@gmail.com>
2017-04-28 09:11:44 -0700

    button wip fix line height

--- spec/src/components/button.js
+++ spec/src/components/button.js
@@ -5,10 +5,10 @@ export default ({inverse}) => (
     <section class={classes({inverse})}>
         <Button icon="cross" label="Flat" flat {...{inverse}}/>
         <Button icon="cross" label="Raised" raised {...{inverse}}/>
-        <Button label="Primary" primary {...{inverse}}/>
+        <Button icon="cross" label="Primary" primary {...{inverse}}/>
         <Button label="Accent" accent {...{inverse}}/>
         <Button icon="cross" floating {...{inverse}}/>
-        <Button floating primary label="3" {...{inverse}}/>
+        <Button label="3" floating primary {...{inverse}}/>
         <Button icon="cross" floating accent mini {...{inverse}}/>
     </section>
 )
