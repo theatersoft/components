769d3f2cc768800f30838757299634648e3cc789
Rob Huehn <rhuehn@gmail.com>
2017-04-30 08:32:40 -0700

    add ripple to button

--- src/button/Button.js
+++ src/button/Button.js
@@ -2,32 +2,34 @@ import {h, Component} from 'preact'
 import {Icon} from '../icon'
 import {classes} from '../classes'
 import style from './button.styl'
+import Ripple from '../ripple'
 
-export class Button extends Component {
-    render (props) {
-        const {
-            accent = false,
-            disabled,
-            floating = false,
-            icon,
-            inverse,
-            label,
-            mini = false,
-            primary = false,
-            raised = false,
-            ...others
-            } = props
-        return (
-            <button class={classes(
+export const Button = Ripple({centered: false})(
+    class extends Component {
+        render (props) {
+            const {
+                accent = false,
+                disabled,
+                floating = false,
+                icon,
+                inverse,
+                label,
+                mini = false,
+                primary = false,
+                raised = false,
+                ...others
+                } = props
+            return (
+                <button class={classes(
                 props.class,
                 primary ? style.primary : accent ? style.accent : style.neutral,
                 raised ?  style.raised : floating ?  style.floating : style.flat,
                 inverse && style.inverse,
                 mini && style.mini
             )} {...{disabled}}>
-                {icon && <Icon icon={icon} class={style.icon} small/>}
-                {label}
-            </button>
-        )
-    }
-}
+                    {icon && <Icon icon={icon} class={style.icon} small/>}
+                    {label}
+                </button>
+            )
+        }
+    })
