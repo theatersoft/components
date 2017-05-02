1eddd14bbc7093a56e16b62e01cf602a89da237e
Rob Huehn <rhuehn@gmail.com>
2017-05-02 10:42:22 -0700

    simplify thumb props

--- src/switch/Thumb.js
+++ src/switch/Thumb.js
@@ -1,8 +1,6 @@
 import {h, Component} from 'preact'
 import style from './switch.styl'
 
-export default ripple => ripple(
-    ({onMouseDown, ...other}) => (
-        <span class={style.thumb} onMouseDown={onMouseDown} {...other} />
-    )
+export default ripple => ripple(props =>
+    <span class={style.thumb} {...props}/>
 )
