5778c1e23b137b582fc349ca3cbf056f706d383e
Rob Huehn <rhuehn@gmail.com>
2017-05-01 21:07:10 -0700

    shorten switch spec

--- spec/src/components/switch.js
+++ spec/src/components/switch.js
@@ -2,23 +2,13 @@ import {h, Component} from 'preact'
 import {Switch} from '@theatersoft/components'
 
 export default class extends Component {
-    state = {
-        sw: false
-    }
-
-    onChange = value => {
-        console.log('Switch.onChange', value)
-        this.setState({sw: value})
-    }
-
     render (p, {sw}) {
-        console.log('Switch.render', p, this.state, sw)
         return (
             <section>
                 <Switch
                     checked={sw}
                     label="Switch"
-                    onChange={this.onChange}
+                    onChange={value => this.setState({sw: value})}
                 />
             </section>
         )
