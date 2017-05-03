28bb78ffbdf6b49d08395181fa9373fa829b925b
Rob Huehn <rhuehn@gmail.com>
2017-05-03 11:00:56 -0700

    remove dup css import from rollup shared source

--- spec/src/App.js
+++ spec/src/App.js
@@ -1,5 +1,4 @@
 import {h} from 'preact'
-import '@theatersoft/components/components.css'
 import Button from './components/button'
 import Icon from './components/icon'
 import Switch from './components/switch'
--- spec/src/index.js
+++ spec/src/index.js
@@ -3,5 +3,6 @@ import './resize'
 import App from './App'
 import './index.styl'
 import '../res/_icons.svg'
+import '@theatersoft/components/components.css'
 
 render(<App />, document.getElementById('ui'))
