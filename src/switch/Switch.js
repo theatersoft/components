8193e72509bb1223b14d9176d4c082d681201e17
Rob Huehn <rhuehn@gmail.com>
2017-05-07 10:37:59 -0700

    simplify Thumb

--- src/switch/Switch.js
+++ src/switch/Switch.js
@@ -1,9 +1,8 @@
 import {h, Component} from 'preact'
-import Ripple from '../ripple'
-import thumbFactory from './Thumb.js'
+import Thumb from './Thumb.js'
 import style from './switch.styl'
 
-const switchFactory = Thumb => class extends Component {
+export const Switch = class extends Component {
     handleToggle = ev => {
         if (!this.props.disabled && this.props.onChange) {
             this.props.onChange(!this.props.checked, ev)
@@ -15,12 +14,10 @@ const switchFactory = Thumb => class extends Component {
             <label class={disabled ? style.disabled : style.field}
                    onClick={this.handleToggle}>
                     <span class={checked ? style.on : style.off}>
-                        {h(Thumb, {disabled})}
+                        <Thumb disabled={disabled}/>
                     </span>
                 {label && <span>{label}</span>}
             </label>
         )
     }
 }
-
-export const Switch = switchFactory(thumbFactory(Ripple({centered: true, spread: 4.2})))
--- src/switch/Thumb.js
+++ src/switch/Thumb.js
@@ -1,6 +1,7 @@
 import {h, Component} from 'preact'
 import style from './switch.styl'
+import Ripple from '../ripple'
 
-export default ripple => ripple(props =>
+export default Ripple({centered: true, spread: 4.2})(props =>
     <span class={style.thumb} {...props}/>
 )
