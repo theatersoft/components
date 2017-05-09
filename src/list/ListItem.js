f5918ceb128da2623c25bf2d61727ccda8337cce
Rob Huehn <rhuehn@gmail.com>
2017-05-09 10:12:06 -0700

    list item action wrapper wip

--- src/list/ListItem.js
+++ src/list/ListItem.js
@@ -4,15 +4,23 @@ import {classes} from '../classes'
 import style from './list.styl'
 import Ripple from '../ripple'
 
-export const ListItem = Ripple({centered: false})(class extends Component {
+const Action = vnode => {
+    const
+        {onClick, onMouseDown} = vnode.attributes,
+        stop = e =>e.stopPropagation()
+    return (
+        <span class={style.action} onMouseDown={stop} onClick={stop}>
+            {vnode}
+        </span>
+    )
+}
+
+export const ListItem = Ripple({centered: false, isRipple: true})(class extends Component {
     render ({icon, label, children, ...props}) {
-        return h('li', {
-                ...props,
-                class: classes(props.class, style.item)
-            },
+        return h('li', {...props, class: classes(props.class, style.item)},
             <span>{icon && <Icon icon={icon} class={style.icon} small disabled={props.disabled}/>}</span>,
             <span>{label}</span>,
-            {children}
+            children && children.map(vnode => !vnode.attributes.isRipple ? Action(vnode) : vnode)
         )
     }
 })
