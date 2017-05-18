a78f09419539019fbebab689aa5761ebc94349ba
Rob Huehn <rhuehn@gmail.com>
2017-05-17 19:30:11 -0700

    enable react devtools

--- spec/rollup/src/index.js
+++ spec/rollup/src/index.js
@@ -8,5 +8,8 @@ const div = document.createElement('div')
 div.innerHTML = svg
 document.body.appendChild(div)
 
+//import 'preact/devtools'
+import {initDevTools} from 'preact/devtools/devtools.js'
+initDevTools()
 import App from '../../src/App'
 render(<App />, document.getElementById('ui'))
