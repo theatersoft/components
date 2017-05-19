eec848615392e30714a9641d27fbe422c562c807
Rob Huehn <rhuehn@gmail.com>
2017-05-18 19:22:59 -0700

    fix tapmenu spec

--- spec/src/components/tapmenu.js
+++ spec/src/components/tapmenu.js
@@ -1,25 +1,16 @@
 import {h, Component} from 'preact'
-import {TapMenu, mousePosition, touchPosition} from '@theatersoft/components'
+import {Button, TapMenu, mousePosition, touchPosition} from '@theatersoft/components'
 import style from '../App.styl'
 
-export default class extends Component {
-    render ({children, ...props}) {
-        const
-            onMouseDown = e => {
-                console.log('onMouseDown', ...mousePosition(e))
-                return props.onMouseDown(e)
-            },
-            onTouchStart = e => {
-                console.log('onTouchStart', ...touchPosition(e))
-                return props.onTouchStart(e)
-            }
-        console.log('free render', children)
-        return (
-            <section class={style.halfscreen}>
-                <TapMenu {...props} {...{onMouseDown, onTouchStart}}>
-                    {children}
-                </TapMenu>
-            </section>
-        )
-    }
+export default () => {
+    return (
+        <section class={style.halfscreen}>
+            <TapMenu>
+                <Button icon="cross" floating/>
+                <Button icon="cross" floating/>
+                <Button icon="cross" floating/>
+                <Button icon="cross" floating/>
+            </TapMenu>
+        </section>
+    )
 }
