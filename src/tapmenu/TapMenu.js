0f0f7af67bd9f1265fed604dcda11a47ee19a411
Rob Huehn <rhuehn@gmail.com>
2017-05-18 18:10:21 -0700

    start TapMenu

---
+++ src/tapmenu/TapMenu.js
@@ -0,0 +1,23 @@
+import {h, Component} from 'preact'
+import {Button, Ripple, mousePosition, touchPosition} from '..'
+import style from './tapmenu.styl'
+
+export const TapMenu = Ripple({centered: false, scaled: false, spread: 100})(class extends Component {
+    render ({children, ...props}) {
+        const
+            onMouseDown = e => {
+                console.log('onMouseDown', ...mousePosition(e))
+                return props.onMouseDown(e)
+            },
+            onTouchStart = e => {
+                console.log('onTouchStart', ...touchPosition(e))
+                return props.onTouchStart(e)
+            }
+        console.log('TapMenu render', children)
+        return (
+            <section class={style.free} {...props} {...{onMouseDown, onTouchStart}}>
+                {children}
+            </section>
+        )
+    }
+})
---
+++ src/tapmenu/index.js
@@ -0,0 +1,1 @@
+export {TapMenu} from './TapMenu'
