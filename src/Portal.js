4261c8bdbf97e8abb42cf167a862a7871a602afb
Rob Huehn <rhuehn@gmail.com>
2017-05-22 12:08:31 -0700

    add portal hoc

---
+++ src/Portal.js
@@ -0,0 +1,30 @@
+import {h, Component, render} from 'preact'
+
+const Null = () => null
+
+export default class extends Component {
+    componentDidUpdate () {this.renderPortal()}
+
+    componentWillUnmount () {this.unrenderPortal()}
+
+    renderPortal () {
+        console.log('renderPortal')
+        const portal = this.props.children && <div class={this.props.class}>{this.props.children}</div>
+        if (portal) {
+            this.portalHostNode = document.body
+            this.portalNode = render(portal, this.portalHostNode, this.portalNode)
+        } else {
+            this.unrenderPortal()
+        }
+    }
+
+    unrenderPortal () {
+        console.log('unrenderPortal')
+        if (this.portalNode) {
+            render(<Null/>, this.portalHostNode, this.portalNode)
+            this.portalNode = null
+        }
+    }
+
+    render () {console.log('Portal.render')}
+}
