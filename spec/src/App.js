ce7a1026385d4d6d9b97920acc9ef1a8994d2239
Rob Huehn <rhuehn@gmail.com>
2017-05-23 11:15:00 -0700

    fix spec section selector

--- spec/src/App.js
+++ spec/src/App.js
@@ -1,20 +1,22 @@
 import {h} from 'preact'
 import Button from './components/button'
+import Dialog from './components/dialog'
 import Icon from './components/icon'
 import TapMenu from './components/tapmenu'
 import List from './components/list'
 import Snackbar from './components/snackbar'
 import Switch from './components/switch'
-import './App.styl'
+import style from './App.styl'
 
 export default () => (
-    h('div', {class: "scroll"}
+    h('div', {class: style.app}
+        ,<Dialog/>
+        //,<Snackbar/>
         //,<List/>
         //,<TapMenu/>
         //,<Switch/>
         //,<Icon/>
         //,<Button/>
-        ,<Snackbar/>
         //,<Button light/>
         //,<Button/>
     )
