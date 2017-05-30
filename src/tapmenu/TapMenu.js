d08fca72878fb60b2f0df9bcc50a96d0b92754f0
Rob Huehn <rhuehn@gmail.com>
2017-05-30 13:29:16 -0700

    tapmenu wip activate grid etc

--- src/tapmenu/TapMenu.js
+++ src/tapmenu/TapMenu.js
@@ -2,23 +2,32 @@ import {h, Component} from 'preact'
 import {classes, Button, Ripple, mousePosition, touchPosition} from '../'
 import style from './tapmenu.styl'
 
+const log = x => (console.log(x), x)
+
 export const TapMenu = Ripple({centered: false, scaled: false, spread: 100})(class extends Component {
-    render ({actions, children, ...props}) {
+    state = {active: false}
+
+    render ({actions, children, ...props}, {active}) {
         const
+            activate = () => this.setState({active: true}),
             onMouseDown = e => {
                 console.log('onMouseDown', ...mousePosition(e))
-                return props.onMouseDown(e)
+                if (!active) {
+                    activate()
+                    props.onMouseDown(e)
+                }
             },
             onTouchStart = e => {
                 console.log('onTouchStart', ...touchPosition(e))
-                return props.onTouchStart(e)
+                props.onTouchStart(e)
             }
         return (
             <div class={style.field} {...props} {...{onMouseDown, onTouchStart}}>
-                <div class={style.group}>
+                <div class={classes(style.group, {[style.active]: active})}>
                     {actions.map((action, i) =>
-                        <Button class={classes(style.button, action.class)} floating mini key={i}
+                        <Button class={classes(style.button, style[`button-${i}`], action.class)} floating mini key={i}
                             {...action}
+                            onClick={e=>console.log(e)}
                         />)}
                 </div>
                 {children}
