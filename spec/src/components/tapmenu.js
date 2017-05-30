9030e93611478a0160f74b03d67415c75ada4004
Rob Huehn <rhuehn@gmail.com>
2017-05-29 21:00:07 -0700

    tapmenu wip

--- spec/src/components/tapmenu.js
+++ spec/src/components/tapmenu.js
@@ -2,15 +2,17 @@ import {h, Component} from 'preact'
 import {Button, TapMenu, mousePosition, touchPosition} from '@theatersoft/components'
 import style from '../App.styl'
 
+const actions = [
+    {tooltip: 'one', icon: 'cross'},
+    {tooltip: 'two', icon: 'cross'},
+    {tooltip: 'three', icon: 'cross'},
+    {tooltip: 'four', icon: 'cross'}
+]
+
 export default () => {
     return (
         <section class={style.halfscreen}>
-            <TapMenu>
-                <Button icon="cross" floating/>
-                <Button icon="cross" floating/>
-                <Button icon="cross" floating/>
-                <Button icon="cross" floating/>
-            </TapMenu>
+            <TapMenu actions={actions}/>
         </section>
     )
 }
--- src/tapmenu/TapMenu.js
+++ src/tapmenu/TapMenu.js
@@ -1,9 +1,9 @@
 import {h, Component} from 'preact'
-import {Button, Ripple, mousePosition, touchPosition} from '..'
+import {classes, Button, Ripple, mousePosition, touchPosition} from '../'
 import style from './tapmenu.styl'
 
 export const TapMenu = Ripple({centered: false, scaled: false, spread: 100})(class extends Component {
-    render ({children, ...props}) {
+    render ({actions, children, ...props}) {
         const
             onMouseDown = e => {
                 console.log('onMouseDown', ...mousePosition(e))
@@ -13,9 +13,14 @@ export const TapMenu = Ripple({centered: false, scaled: false, spread: 100})(cla
                 console.log('onTouchStart', ...touchPosition(e))
                 return props.onTouchStart(e)
             }
-        console.log('TapMenu render', children)
         return (
             <div class={style.field} {...props} {...{onMouseDown, onTouchStart}}>
+                <div class={style.group}>
+                    {actions.map((action, i) =>
+                        <Button class={classes(style.button, action.class)} floating mini key={i}
+                            {...action}
+                        />)}
+                </div>
                 {children}
             </div>
         )
