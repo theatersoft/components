f7041942077dd2aa0a0c9b3c753359c188e1d7bf
Rob Huehn <rhuehn@gmail.com>
2017-06-01 17:17:16 -0700

    cleanup render handlers

--- src/tapmenu/TapMenu.js
+++ src/tapmenu/TapMenu.js
@@ -7,29 +7,37 @@ const log = (...x) => (console.log(...x), x[0])
 export const TapMenu = Ripple({centered: false, scaled: false, spread: 100})(class extends Component {
     state = {active: false}
 
+    activate = (left, top) => {
+        this.setState({active: false, left, top}, () => {
+            this.activateTimeout = setTimeout(() => this.setState({active: true}), 100)
+        })
+    }
+
+    deactivate = () => {
+        this.setState({active: false})
+    }
+
+    onMouseDown = e => {
+        console.log('onMouseDown', ...mousePosition(e))
+        if (!this.state.active) {
+            this.activate(...mousePosition(e))
+            this.props.onMouseDown(e)
+        } else this.deactivate()
+    }
+
+    onTouchStart = e => {
+        console.log('onTouchStart', ...touchPosition(e))
+        //    props.onTouchStart(e)
+    }
+
+    onClick = e => {
+        log(e)
+        deactivate()
+    }
+
     render ({actions, children, ...props}, {active, left, top}) {
-        const
-            activate = (left, top) => {
-                this.setState({active: false, left, top}, () => {
-                    this.activateTimeout = setTimeout(() => this.setState({active: true}), 100)
-                })
-            },
-            deactivate = () => {
-                this.setState({active: false})
-            },
-            onMouseDown = e => {
-                console.log('onMouseDown', ...mousePosition(e))
-                if (!active) {
-                    activate(...mousePosition(e))
-                    props.onMouseDown(e)
-                } else deactivate()
-            },
-            onTouchStart = e => {
-                console.log('onTouchStart', ...touchPosition(e))
-                //    props.onTouchStart(e)
-            }
         return (
-            <div class={style.field} {...props} {...{onMouseDown, onTouchStart}}>
+            <div class={style.field} {...props} onMouseDown={this.onMouseDown} onTouchStart={this.onTouchStart}>
                 <div
                     class={classes(style.group, {[style.active]: active})}
                     style={{left, top}}
@@ -39,7 +47,7 @@ export const TapMenu = Ripple({centered: false, scaled: false, spread: 100})(cla
                             class={classes(style.button, style[`button-${i}`], action.class)}
                             round inverse key={i}
                             {...action}
-                            onClick={e => deactivate()}
+                            onClick={this.onClick}
                         />)}
                 </div>
                 {children}
