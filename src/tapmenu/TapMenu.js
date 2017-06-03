9ea519a34f50886b11c47b3ce54749a13f44f593
Rob Huehn <rhuehn@gmail.com>
2017-06-03 12:04:27 -0700

    publish tapmenu active state

--- src/tapmenu/TapMenu.js
+++ src/tapmenu/TapMenu.js
@@ -1,9 +1,15 @@
 import {h, Component} from 'preact'
 import {classes, Button, Ripple, mousePosition, touchPosition} from '../'
 import style from './tapmenu.styl'
+import {log} from '@theatersoft/bus'
 
 export const TapMenu = Ripple({centered: false, scaled: false, spread: 100})(class extends Component {
     state = {active: false}
+
+    componentDidUpdate (_prevProps, prevState) {
+        if (this.state.active !== prevState.active)
+            if (this.props.onActive) this.props.onActive(this.state.active)
+    }
 
     activate = (left, top) => {
         this.setState({active: false, left, top}, () => {
@@ -23,7 +29,6 @@ export const TapMenu = Ripple({centered: false, scaled: false, spread: 100})(cla
     }
 
     onTouchStart = e => {
-        console.log('onTouchStart', ...touchPosition(e))
         //    props.onTouchStart(e)
     }
 
@@ -33,7 +38,7 @@ export const TapMenu = Ripple({centered: false, scaled: false, spread: 100})(cla
 
     render ({actions, children, ...props}, {active, left, top}) {
         return (
-            <div  {...props} class={classes(style.field, props.class)} onMouseDown={this.onMouseDown} onTouchStart={this.onTouchStart} onClick={this.onFieldClick}>
+            <div {...props} class={classes(style.field, props.class)} onMouseDown={this.onMouseDown} onTouchStart={this.onTouchStart} onClick={this.onFieldClick}>
                 <div class={classes(style.group, active && style.active)} style={{left, top}}>
                     <Button class={classes(style.button, style[`button-x`])} small round inverse onClick={this.onFieldClick}/>
                     {actions.map((action, i) =>
