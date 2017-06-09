6ac264c42ad63e1e5673b7b3a6f93759c5f76420
Rob Huehn <rhuehn@gmail.com>
2017-06-09 12:10:23 -0700

    sheet content experiment

--- spec/src/components/sheet.js
+++ spec/src/components/sheet.js
@@ -2,20 +2,25 @@ import {h, Component} from 'preact'
 import {Button, Sheet} from '@theatersoft/components'
 
 export default class extends Component {
-    state = {active: false}
+    state = {active: false, next: false}
 
-    render (_, {active, type}) {
+    render (_, {active, type, next}) {
         return (
             <section>
                 <p>sheet</p>
                 <nav>
-                <Button label="Left" raised primary onClick={() => this.setState({active: true, type: 'left'})}/>
-                <Button label="Right" raised primary onClick={() => this.setState({active: true, type: 'right'})}/>
-                <Button label="Top" raised primary onClick={() => this.setState({active: true, type: 'top'})}/>
-                <Button label="Bottom" raised primary onClick={() => this.setState({active: true, type: 'bottom'})}/>
+                    <Button label="Left" raised primary onClick={() => this.setState({active: true, type: 'left'})}/>
+                    <Button label="Right" raised primary onClick={() => this.setState({active: true, type: 'right'})}/>
+                    <Button label="Top" raised primary onClick={() => this.setState({active: true, type: 'top'})}/>
+                    <Button label="Bottom" raised primary
+                            onClick={() => this.setState({active: true, type: 'bottom'})}/>
                 </nav>
-                <Sheet active={active} type={type} onClick={() => this.setState({active: false})}>
-                    <p>content</p>
+                <Sheet active={active} type={type} onClick={() => this.setState({active: false, next: false})}>
+                    {next ? <p>next content</p> : [
+                        <p>content</p>,
+                        < Button label="Next" raised primary
+                                 onClick={() => this.setState({active: true, type: 'left', next: true})}/>
+                    ]}
                 </Sheet>
             </section>
         )
