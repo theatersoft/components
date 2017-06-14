8d2582163813145b706c4f4e86d230749fcd8aa8
Rob Huehn <rhuehn@gmail.com>
2017-06-14 15:27:16 -0700

    add flex align

--- src/flex/Flex.js
+++ src/flex/Flex.js
@@ -2,20 +2,22 @@ import {h, Component} from 'preact'
 import {classes} from '../'
 import style from './flex.styl'
 
-export const Row = ({class: _class, end, center, between, around, children}) =>
+export const Row = ({class: _class, end, center, between, around, alignstart, alignend, aligncenter, alignbaseline, children}) =>
     <div class={classes(
         style.row,
         _class,
-        end ? style.end : center ? style.center : between ? style.between : around && style.around
+        end ? style.end : center ? style.center : between ? style.between : around && style.around,
+        alignstart ? style.alignstart : alignend ? style.alignend : aligncenter ? style.aligncenter : alignbaseline && style.alignbaseline
     )}>
         {children}
     </div>
 
-export const Col = ({class: _class, end, center, between, around, children}) =>
+export const Col = ({class: _class, end, center, between, around, alignstart, alignend, aligncenter, children}) =>
     <div class={classes(
         style.col,
         _class,
-        end ? style.end : center ? style.center : between ? style.between : around && style.around
+        end ? style.end : center ? style.center : between ? style.between : around && style.around,
+        alignstart ? style.alignstart : alignend ? style.alignend : aligncenter && style.aligncenter
     )}>
         {children}
     </div>
