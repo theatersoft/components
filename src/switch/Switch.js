f886b9933c9d55f96187733df2eaa7229005a813
Rob Huehn <rhuehn@gmail.com>
2017-06-20 10:43:11 -0700

    fix switch props passthrough, add input element for currentTarget

--- src/switch/Switch.js
+++ src/switch/Switch.js
@@ -1,22 +1,29 @@
 import {h, Component} from 'preact'
+import {classes} from '../'
 import Thumb from './Thumb.js'
 import style from './switch.styl'
 
 export const Switch = class extends Component {
-    handleToggle = e => {
+    onClick = e => {
         if (!this.props.disabled && this.props.onChange) {
             this.props.onChange(!this.props.checked, e)
         }
-        e.stopPropagation()
+        //e.stopPropagation()
     }
 
-    render ({label, checked = false, disabled = false}) {
+    render ({class: _class, label, checked = false, disabled = false, onChange, ...props}) {
         return (
-            <label class={disabled ? style.disabled : style.field}
-                   onClick={this.handleToggle}>
-                    <span class={checked ? style.on : style.off}>
-                        <Thumb disabled={disabled}/>
-                    </span>
+            <label class={classes(_class, disabled ? style.disabled : style.field)}>
+                <input
+                    {...props}
+                    type="checkbox"
+                    checked={checked}
+                    class={style.input}
+                    onClick={this.onClick}
+                />
+                <span class={checked ? style.on : style.off}>
+                    <Thumb disabled={disabled}/>
+                </span>
                 {label && <span>{label}</span>}
             </label>
         )
