a95c33405f050cf2d1ffdf8d05302cc7eb233279
Rob Huehn <rhuehn@gmail.com>
2017-06-21 11:10:28 -0700

    add listitem hover background color

--- src/list/ListItem.js
+++ src/list/ListItem.js
@@ -17,7 +17,7 @@ const Action = vnode => {
 export const ListItem = Ripple({centered: false, isRipple: true})(class extends Component {
     render ({icon, label, children, ...props}) {
         return (
-            <li class = {classes(props.class, style.item)} {...props}>
+            <li class={classes(props.class, style.item)} {...props}>
                 {icon && <Icon icon={icon} class={style.icon} small disabled={props.disabled}/>}
                 <span class={style.content}>
                     <span class={style.text}>
