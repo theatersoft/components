989023cc0b2e581f19d7e6d18da3a8fca4a286d9
Rob Huehn <rhuehn@gmail.com>
2017-10-11 10:26:48 -0700

    slider wip

--- spec/src/components/slider.js
+++ spec/src/components/slider.js
@@ -2,17 +2,15 @@ import {h, Component} from 'preact'
 import {Slider} from '@theatersoft/components'
 
 export default class extends Component {
-    onChange = value => {
-        this.setState({value})
-    }
+    state = {value: 25}
+
+    onChange = value => this.setState({value})
 
     render (_, {value}) {
         return (
             <section>
-                <Slider
-                    value={value}
-                    onChange={this.onChange}
-                />
+                <span>{value}</span>
+                <Slider value={value} onChange={this.onChange}/>
             </section>
         )
     }
--- src/slider/Slider.js
+++ src/slider/Slider.js
@@ -3,18 +3,33 @@ import {classes} from '../'
 import style from './slider.styl'
 
 export const Slider = class extends Component {
+    static defaultProps = {min: 0, max: 100}
+
+    componentDidMount() {
+        window.addEventListener('resize', this.resize)
+        this.resize()
+    }
+
+    // trackC
+
+    resize = e => {
+        const {left, right} = this.trackC.getBoundingClientRect()
+        console.log(this.trackC.getBoundingClientRect())
+    }
+
     onClick = e => {
     }
 
-    render ({class: _class, label, checked = false, disabled = false, onChange, ...props}) {
+    render ({class: _class, value, min, max, onChange, ...props}) {
+        const scaled = (value - min) / (max - min)
         return (
-            <div class="slider">
-                <div class="track-container">
-                    <div class="track" style="transform: scaleX(0.5);"></div>
+            <div class={style.slider}>
+                <div class={style.trackC} ref={node => this.trackC = node}>
+                    <div class={style.track} style={`transform: scaleX(${scaled});`}/>
                 </div>
-                <div class="thumb-container" style="transform: translateX(300px) translateX(-50%);">
-                    <svg class="thumb" width="21" height="21">
-                        <circle cx="10.5" cy="10.5" r="7.875"></circle>
+                <div class={style.thumbC} style="transform: translateX(390px);">
+                    <svg class={style.thumb}>
+                        <circle cx="10.5" cy="10.5" r="7.875"/>
                     </svg>
                 </div>
             </div>
