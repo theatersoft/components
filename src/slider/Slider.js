78179974dc293e53879616506afdf288256dd7dd
Rob Huehn <rhuehn@gmail.com>
2017-10-12 12:50:13 -0700

    slider fix positioning wip

--- src/slider/Slider.js
+++ src/slider/Slider.js
@@ -5,7 +5,7 @@ import style from './slider.styl'
 export const Slider = class extends Component {
     static defaultProps = {min: 0, max: 100}
 
-    componentDidMount() {
+    componentDidMount () {
         window.addEventListener('resize', this.resize)
         this.resize()
     }
@@ -13,24 +13,23 @@ export const Slider = class extends Component {
     // trackC
 
     resize = e => {
-        const {left, right} = this.trackC.getBoundingClientRect()
+        const {left, width} = this.trackC.getBoundingClientRect()
         console.log(this.trackC.getBoundingClientRect())
+        this.setState({left, width})
     }
 
     onClick = e => {
     }
 
-    render ({class: _class, value, min, max, onChange, ...props}) {
+    render ({value, min, max, onChange, class: _class}, {left, width}) {
         const scaled = (value - min) / (max - min)
         return (
-            <div class={style.slider}>
+            <div class={classes(style.slider, _class)}>
                 <div class={style.trackC} ref={node => this.trackC = node}>
-                    <div class={style.track} style={`transform: scaleX(${scaled});`}/>
+                    <div class={style.track} style={{transform: `scaleX(${scaled})`}}/>
                 </div>
-                <div class={style.thumbC} style="transform: translateX(390px);">
-                    <svg class={style.thumb}>
-                        <circle cx="10.5" cy="10.5" r="7.875"/>
-                    </svg>
+                <div class={style.thumbC} style={{transform: `translateX(${scaled * width}px)`}}>
+                    <div class={style.thumb}/>
                 </div>
             </div>
         )
