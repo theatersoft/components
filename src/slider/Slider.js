284ac74d2f10695ddac75163fbb70b1bfebefa78
Rob Huehn <rhuehn@gmail.com>
2017-10-12 20:10:45 -0700

    impl slider mouse down move up wip

--- src/slider/Slider.js
+++ src/slider/Slider.js
@@ -12,26 +12,68 @@ export const Slider = class extends Component {
 
     componentWillUnmount () {
         window.removeEventListener('resize', this.resize)
+        document.removeEventListener('mousemove', this.mouseMove)
+        document.removeEventListener('mouseup', this.mouseUp)
     }
 
-    // trackC
-
-    resize = e => {
+    resize = (e, cb) => {
         const {left, width} = this.trackC.getBoundingClientRect()
-        this.setState({left, width})
+        this.setState({left, width}, cb)
     }
 
-    onClick = e => {
+    mouseDown = e => {
+        document.addEventListener('mousemove', this.mouseMove)
+        document.addEventListener('mouseup', this.mouseUp)
+        this.start(e.pageX)
     }
+
+    mouseMove = e => {
+        this.move(e.pageX)
+    }
+
+    mouseUp = e => {
+        document.removeEventListener('mousemove', this.mouseMove)
+        document.removeEventListener('mouseup', this.mouseUp)
+        this.end()
+    }
+
+    start (x) {
+        this.resize(undefined, () => {
+            this.setState({pressed: true})
+            this.props.onChange(this.value(x))
+        })
+    }
+
+    move (x) {
+        const value = this.value(x)
+        if (value !== this.props.value) this.props.onChange(value)
+    }
+
+    end () {
+        this.setState({pressed: false})
+    }
+
+    value (x) {
+        const
+            {left, width} = this.state,
+            {max, min, step} = this.props,
+            val = ((x - left) / width) * (max - min) // TODO step
+        return val + min
+    }
+
+    refTrackC = node => this.trackC = node
 
     render ({value, min, max, onChange, class: _class}, {left, width}) {
-        const scaled = (value - min) / (max - min)
+        const
+            scaled = (value - min) / (max - min)
         return (
             <div class={classes(style.slider, _class)}>
-                <div class={style.trackC} ref={node => this.trackC = node}>
+                <div class={style.trackC} ref={this.refTrackC}>
                     <div class={style.track} style={{transform: `scaleX(${scaled})`}}/>
                 </div>
-                <div class={style.thumbC} style={{transform: `translateX(${scaled * width}px)`}}>
+                <div class={style.thumbC}
+                     style={{transform: `translateX(${scaled * width}px)`}}
+                     onMouseDown={this.mouseDown}>
                     <div class={style.thumb}/>
                 </div>
             </div>
